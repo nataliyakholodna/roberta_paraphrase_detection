d05cf34ccf05d91a4e8bb75a4f9d5045c952a281
nataliyakholodna <kholodnaya.nataliya@gmail.com>
2022-09-09 15:12:43 +0200

    added Flask, read,e

---
+++ READMe.md
@@ -0,0 +1,8 @@
+# Hi!
+This app is used as supplementary component for paraphrase_detection app.
+Is uses trained Roberta model, PAWS dataset was used for its pretraining.
+Weights of the model have size of 1.5GB and are not uploaded to this repo. 
+
+App is based on Flask which allows to get model's prediction via POST-request.
+
+Updates are coming soon!
--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from transformers import AutoTokenizer
 import torch
 from model import Classifier_Model
 from transformers import logging
+from flask import Flask, jsonify, request
+
 logging.set_verbosity_error()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -12,16 +14,15 @@ sent2 = 'Bradd Crellin also represented BARLA Great Britain on a tour through Au
         'with 6 other players representing Cumbria . '
 
 config = {
-        'model_name': 'roberta-base',
-        'dataset': 'PAWS' ,  # ['MRPC', 'Quora', 'PAWS']
-        'batch_size': 64,
-        'epochs': 100,
-        'num_classes': 1,
-        'lr': 1.5e-6,
-        'warmup_percentage': 0.2,
-        'w_decay': 0.001,
+    'model_name': 'roberta-base',
+    'dataset': 'PAWS',  # ['MRPC', 'Quora', 'PAWS']
+    'batch_size': 64,
+    'epochs': 100,
+    'num_classes': 1,
+    'lr': 1.5e-6,
+    'warmup_percentage': 0.2,
+    'w_decay': 0.001,
 }
-
 
 model_name = 'roberta-base'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -29,4 +30,29 @@ model_roberta = Classifier_Model.load_from_checkpoint('train/weights-50-epochs.c
 model_roberta.to(device)
 
 
-roberta_prediction(sent1, sent2, tokenizer, model_roberta, device)
+app = Flask(__name__)
+
+
+@app.post('/predict')
+def predict():
+    data = request.json
+    try:
+        s1 = data['s1']
+        s2 = data['s2']
+    except KeyError:
+        return jsonify({'key': 'incorrect data'})
+
+    proba, prediction = roberta_prediction(s1, s2,
+                                           tokenizer,
+                                           model_roberta,
+                                           device)
+    try:
+        result = jsonify({'probability': proba, 'prediction': prediction})
+
+    except TypeError as err:
+        return jsonify({'error': str(err)})
+    return result
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
